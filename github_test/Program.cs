67fe01d4786799830ee211e1922ab719d6d38364
Mahmudi <@ArsenPetrossov>
2024-01-30 16:28:30 +0500

    feat: add sum of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -4,6 +4,13 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        
+        Console.WriteLine("Введите первое число:");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        double sum = firstNumber + secondNumber;
+        Console.WriteLine("Результат сложения " + sum);
     }
 }
